a771e08b6911d8e9c08e1a3a9c4c7df9d3ff80fa
lschuering <luke.schuering@gmail.com>
2022-11-14 16:56:28 -0800

    initial solutions

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
  *
  * @returns A promise that is designed to resolve with a list of hobbits, or potentially fail with an failure object. The failure object includes a boolean success property and a string message property.
  */
-function getList() {
+async function getList() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       let potentialFail = Math.round(Math.random() * 100) < 10;
@@ -15,10 +15,45 @@ function getList() {
   });
 }
 
-// TODO: Handle the resolved or rejected states of the promise
+let errPar = document.querySelector("#error");
+let uoList = document.querySelector("#list");
 
+// let promise = getList();
+
+function handleList(list) {
+  console.log(list);
+  list.forEach((hobbit) => {
+    let li = document.createElement("li");
+    li.textContent = hobbit;
+    uoList.appendChild(li);
+  });
+}
+
+function handleError(err) {
+  console.error(err);
+  errPar.textContent = err.message;
+}
+
+// promise.then(handleList).catch(handleError);
+async function updateDOMList() {
+  try {
+    let list = await getList();
+    list.forEach((hobbit) => {
+      let li = document.createElement("li");
+      li.textContent = hobbit;
+      uoList.appendChild(li);
+    });
+    console.log(list);
+  } catch (err) {
+    console.error(err);
+    errPar.textContent = err.message;
+  }
+}
+
+updateDOMList();
+
+// TODO: Handle the resolved or rejected states of the promise
 // TODO: If the promise resolves with the list of hobbits
 // Render the list of hobbits as list items within the unordered list with id="list" (check the index.html file)
-
 // TODO: If the promise rejects with the failure object
 // Display the failure message in the paragraph element with id="error" (check index.html file)
